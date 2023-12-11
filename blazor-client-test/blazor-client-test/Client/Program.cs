88ee2ef78e3e4b3630f8570e927e178203f12780
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2023-12-11 14:33:20 +0100

    .

---
+++ blazor-client-test/blazor-client-test/Client/Program.cs
@@ -0,0 +1,17 @@
+using blazor_client_test;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace blazor_client_test {
+    public class Program {
+        public static async Task Main(string[] args) {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}

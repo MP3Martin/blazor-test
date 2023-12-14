818434fda241b7e7a06aa79663956d369028d31d
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2023-12-14 12:06:47 +0100

    .

--- blazor-client-test/blazor-client-test/Pages/MyCode2.razor.cs
+++ blazor-client-test/blazor-client-test/Pages/MyCode2.razor.cs
@@ -3,27 +3,58 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.JSInterop;
 
 namespace blazor_client_test.Pages {
-    public partial class MyCode2 {
-        [Inject]
-        public IJSRuntime? JSRuntime { get; set; }
-        private static string textOut = "";
-        private static int snakeArraySize = 30;
-        private void ChangeDirection(KeyboardEventArgs e) {
-            string key = e.Key;
-            JSRuntime.InvokeVoidAsync("console.log", key);
-            JSRuntime.InvokeVoidAsync("clearSnakeControlInput");
-        }
+	public partial class MyCode2 {
+		[Inject]
+		public IJSRuntime? JSRuntime { get; set; }
+		private static string textOut = "";
+		private static int snakeArraySize = 30;
+		private void ChangeDirection(KeyboardEventArgs e) {
+			string key = e.Key;
+			char keyChar = key.ToLower().ToCharArray()[0];
+			switch (keyChar) {
+				case 'w' or 'a' or 's' or 'd':
+					snakeGame.lastKey = keyChar;
+					JSRuntime.InvokeVoidAsync("console.log", keyChar.ToString());
+					break;
+			}
+			JSRuntime.InvokeVoidAsync("clearSnakeControlInput");
+		}
 
-        private int[,] snakeArray = new int[snakeArraySize, snakeArraySize];
-        private void OnPageLoad() {
-            App.CreateTimer(() => {
-                for (int i = 0; i < snakeArray.GetLength(0); i++) {
-                    for (int j = 0; j < snakeArray.GetLength(1); j++) {
-                        snakeArray[i, j] = new Random().Next(0, 1 + 1);
-                    }
-                }
-                InvokeAsync(StateHasChanged);
-            }, 1);
-        }
-    }
+		private class SnakeGame {
+			private static (int, int) gameSize = (0, 0);
+			public SnakeGame((int, int) gameSizeArg) {
+				gameSize = gameSizeArg;
+				snakeArray = new int[gameSize.Item1, gameSize.Item2];
+				snakeCoords.Add(((gameSize.Item1 / 2, gameSize.Item2 / 2), 1));
+			}
+			public int[,] snakeArray = new int[gameSize.Item1, gameSize.Item2];
+			// ((snakeX, snakeY), data)
+			// data: 1 = snake
+			private List<((int, int), int)> snakeCoords = new();
+			public char lastKey = new();
+			public void updateSnake() {
+				if (lastKey == 'w') {
+					snakeCoords.Add(((1, 1), 1));
+				} else {
+					snakeCoords.Clear();
+				}
+				snakeArray = new int[gameSize.Item1, gameSize.Item2];
+				for (int i = 0; i < snakeArray.GetLength(0); i++) {
+					for (int j = 0; j < snakeArray.GetLength(1); j++) {
+						if (snakeCoords.Contains(((i, j), 1))) {
+							snakeArray[i, j] = 1;
+						}
+					}
+				}
+			}
+		}
+
+		private SnakeGame snakeGame = new((snakeArraySize, snakeArraySize));
+		private void OnPageLoad() {
+			App.CreateTimer(() => {
+				snakeGame.updateSnake();
+				InvokeAsync(StateHasChanged);
+			}, 50);
+		}
+	}
 }

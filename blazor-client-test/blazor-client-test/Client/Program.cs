5f4212b6e10db4395c145e051b0f713c7d45955f
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2023-12-14 11:21:25 +0100

    .

--- blazor-client-test/blazor-client-test/Client/Program.cs
+++ blazor-client-test/blazor-client-test/Client/Program.cs
@@ -1,4 +1,3 @@
-using blazor_client_test;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -10,7 +9,7 @@ namespace blazor_client_test {
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            
+
             await builder.Build().RunAsync();
         }
     }
--- blazor-client-test/blazor-client-test/Pages/MyCode2.razor.cs
+++ blazor-client-test/blazor-client-test/Pages/MyCode2.razor.cs
@@ -3,24 +3,27 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.JSInterop;
 
 namespace blazor_client_test.Pages {
-	public partial class MyCode2 {
-		[Inject]
-		public IJSRuntime? JSRuntime { get; set; }
-		private string textOut = "";
-		private void changeDirection(KeyboardEventArgs e) {
-			string key = e.Key;
-			JSRuntime.InvokeVoidAsync("console.log", key);
-			JSRuntime.InvokeVoidAsync("clearSnakeControlInput");
-		}
+    public partial class MyCode2 {
+        [Inject]
+        public IJSRuntime? JSRuntime { get; set; }
+        private static string textOut = "";
+        private static int snakeArraySize = 30;
+        private void ChangeDirection(KeyboardEventArgs e) {
+            string key = e.Key;
+            JSRuntime.InvokeVoidAsync("console.log", key);
+            JSRuntime.InvokeVoidAsync("clearSnakeControlInput");
+        }
 
-		private int[,] snakeArray = new int[30, 30];
-		public void start() {
-			for (int i = 0; i < snakeArray.GetLength(0); i++) {
-				for (int j = 0; j < snakeArray.GetLength(1); j++) {
-					snakeArray[i, j] = new Random().Next(0, 1 + 1);
-				}
-			}
-
-		}
-	}
+        private int[,] snakeArray = new int[snakeArraySize, snakeArraySize];
+        private void OnPageLoad() {
+            App.CreateTimer(() => {
+                for (int i = 0; i < snakeArray.GetLength(0); i++) {
+                    for (int j = 0; j < snakeArray.GetLength(1); j++) {
+                        snakeArray[i, j] = new Random().Next(0, 1 + 1);
+                    }
+                }
+                InvokeAsync(StateHasChanged);
+            }, 1);
+        }
+    }
 }
